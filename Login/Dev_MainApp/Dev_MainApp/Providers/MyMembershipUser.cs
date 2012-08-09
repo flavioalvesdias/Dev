482711a5d22597bdb9cc6f503e470ea0bff7f406
flavioalvesdias <flavio.adias@gmail.com>
2012-08-08 21:52:52 -0300

    Commit

---
+++ Login/Dev_MainApp/Dev_MainApp/Providers/MyMembershipUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+namespace Dev_MainApp.Providers
+{
+    public class MyMembershipUser : MembershipUser
+    {
+        public MyMembershipUser(long userId, string userName, string email)
+            : base(Membership.Provider.Name, userName, userId, email, null, null, true, false, DateTime.Now, DateTime.Now, DateTime.Now, DateTime.Now, DateTime.Now)
+        {
+        }
+    }
+}
