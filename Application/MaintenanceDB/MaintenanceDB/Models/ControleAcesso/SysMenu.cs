de4c34a84d81f754950d90dcbe46896832a48aa0
flavioalvesdias <flavio.adias@gmail.com>
2013-08-25 15:01:28 -0300

    Application

---
+++ Application/MaintenanceDB/MaintenanceDB/Models/ControleAcesso/SysMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaintenanceDB.Models
+{
+    /// <summary>
+    /// Author Flávio Dias
+    /// Classe referente aos Menus que a aplicação terá
+    /// </summary>
+    [Table("SysMenu")]
+    public class SysMenu
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int MenuId { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [MaxLength(100), MinLength(3, ErrorMessage = "Descrição deverá ter entre 3 e 100 caracteres.")]
+        public string Descricao { get; set; }
+        public int? MenuPaiId { get; set; }
+
+        [ForeignKey("MenuPaiId")]
+        public virtual SysMenu MenuPai { get; set; }
+        public string CreateUserId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string UpdateUserId { get; set; }
+        public Boolean Active { get; set; }
+
+        
+    }
+}
---
+++ Application/MaintenanceDB/MaintenanceDB/Models/ControleAcesso/SysView.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaintenanceDB.Models
+{
+    /// <summary>
+    /// Author Flávio Dias
+    /// Classe responsável por conter dados sobre todas as visões que existiram no sistema
+    /// Apenas visões cadastradas serão mostradas no menu da aplicação
+    /// </summary>
+    
+    [Table("SysView")]
+    public class SysView
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ViewId { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [MaxLength(100)]
+        public string Descricao { get; set; }
+
+        [MaxLength(50)]
+        [Required(ErrorMessage = "*")]
+        public string ControllerName { get; set; }
+
+        [MaxLength(25)]
+        [Required(ErrorMessage = "*")]
+        public string ActionName { get; set; }
+
+        public string CreateUserId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string UpdateUserId { get; set; }
+        public Boolean Active { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public int MenuId { get; set; }
+
+        [ForeignKey("MenuId")]
+        public virtual SysMenu sysMenu { get; set; }
+    }
+}
